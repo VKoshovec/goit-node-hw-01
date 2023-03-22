d10715793b6ebc524f2a6cfe99c3cea6676eb2b2
VladimirKoshovec <koshavladmir@gmail.com>
2023-03-22 11:22:19 +0200

    start

---
+++ contacts.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const contactsPath  = path.dirname('./db/contacts.json');
+// require('contacts.json')
+console.log(contactsPath);
